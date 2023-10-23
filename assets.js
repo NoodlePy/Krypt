b0b2b7900af28972a69b9360d00b6197d65b022e
FallLikeAThunderbolt <extremelycoolwindow101@gmail.com>
2023-10-24 00:23:35 +0530

    Adding Assets Made Possible with proper Insertion

--- assets.js
+++ assets.js
@@ -7,17 +7,35 @@ const invis = document.querySelector(".invis")
 
 
 add_btn.addEventListener("click", function() {
-    
-    const asset_cont = document.createElement("div")
-    main_cont.appendChild(asset_cont)
-    asset_cont.classList.add("inp_field")
-    asset_cont.classList.add("asset_margin")
-    const crypto_label = document.createElement("label")
-    asset_cont.appendChild(crypto_label)
-    crypto_label.classList.add("crypto")
-    crypto_label.textContent = inp_crypto.value
-    const value_label = document.createElement("label")
-    asset_cont.appendChild(value_label)
-    value_label.classList.add("amount")
-    value_label.textContent = inp_value.value
+    if (inp_value.value && !(inp_value.value == 0)) {
+        const asset_cont = document.createElement("div")
+        main_cont.insertBefore(asset_cont,invis)
+        asset_cont.classList.add("inp_field")
+        asset_cont.classList.add("asset_margin")
+
+        const crypto_div = document.createElement("div")
+        const crypto_label = document.createElement("p")
+        crypto_div.appendChild(crypto_label)
+        asset_cont.appendChild(crypto_div)
+
+        crypto_div.classList.add("asset_crypto")
+        crypto_label.textContent = inp_crypto.value
+        crypto_label.classList.add("crypto_text")
+
+
+        const value_div = document.createElement("div")
+        const value_label = document.createElement("label")
+        value_div.appendChild(value_label)
+        asset_cont.appendChild(value_div)
+        value_div.classList.add("amount")
+        value_label.textContent = inp_value.value
+        value_label.classList.add("crypto_text")
+
+        const edit_btn = document.createElement("button")
+        asset_cont.appendChild(edit_btn)
+        edit_btn.innerHTML = "Edit"
+        edit_btn.classList.add("edit")
+
+        main_cont.insertBefore(invis,asset_cont)
+    }
 })
